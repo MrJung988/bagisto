ed469cd6d5c18cfa29bab85a7ba9bcc0a829cd5e
Samir Karki <karkisamir99@gmail.com>
2022-08-18 12:55:21 +0545

    Ready for Advertisement Banner

--- packages/ACME/TestPackage/src/Resources/views/admin/index.blade.php
+++ packages/ACME/TestPackage/src/Resources/views/admin/index.blade.php
@@ -1,7 +1,7 @@
 @extends('admin::layouts.master')
 
 @section('page_title')
-    Package TestPackage
+    Add Advertisement Banner
 @stop
 
 @section('content-wrapper')
@@ -9,16 +9,15 @@
     <div class="content full-page dashboard">
         <div class="page-header">
             <div class="page-title">
-                <h1>Package TestPackage</h1>
+                <h1>Add Advertisement Banner</h1>
             </div>
 
             <div class="page-action">
             </div>
         </div>
+        
+        <div class="page-content">            
 
-        <div class="page-content">
-            
-<datagrid-plus src="{{ route('admin.catalog.products.index') }}"></datagrid-plus>
         </div>
     </div>
 
